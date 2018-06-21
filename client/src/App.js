842929bafc784f1b3a96cc96419af5a11ccf96ad
chriserb614 <chris.erbstoesser@yahoo.com>
2018-06-20 20:30:26 -0700

    Added React-Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./App.css";
-// import Feed from "./pages/feed";
+import Feed from "./pages/feed";
 import Register from "./pages/newUser"
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Register />
+      <Router>
+        <Fragment>
+          <Route exact path="/feed" component= {Feed}/>
+          <Route exact path="/register" component= {Register}/>
 
-      </Fragment>
-    );
+        </Fragment>
+
+      </Router>
+
+  );
   }
-}
+  }
+
+        
+        
+      
 
 export default App;
