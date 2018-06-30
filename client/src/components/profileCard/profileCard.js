2370ecbbb17c9236d5c48383e8092d3a44c4650f
Josiah Pham <josiah.pham@gmail.com>
2018-06-30 13:57:01 -0700

    connected backend login authenication with profileCard component

--- client/src/components/profileCard/profileCard.js
+++ client/src/components/profileCard/profileCard.js
@@ -1,9 +1,29 @@
 import React, { Component, Fragment } from "react";
 import logo from '../../images/fish.jpg';
 import "./profileCard.css";
+import {USER} from "../../utils"
+const URL ='http://localhost:3001/'
 
 
 class profileCard extends Component {
+    state = {
+        trails: [],
+        isLoggedIn: sessionStorage.isLoggedIn,
+        userId: sessionStorage.userId,
+        userAc:[]
+      }; 
+      componentDidMount(){
+        this.loadUser(this.state.userId)
+      }
+    loadUser =(id)=>{
+        USER.getUser(id)
+            .then(res=>{
+                this.setState({userAc: res.data})
+                console.log(res.data)
+            })
+    }
+  
+
     render() {
         return (
         <Fragment>
@@ -11,12 +31,12 @@ class profileCard extends Component {
                 <div className="col s3"></div>
                     <div className="col s12 m6">
                         <div className="card blue-grey darken-1">
-                            <img src={logo}  className="circle profilePic" alt="Logo" />;
+                            <img src={logo}  className="circle profilePic" alt="Logo" />
                             <div className="card-content white-text center-align">
                                 {/* data for username goes here */}
-                                <span className="card-title">Username</span>
+                                <span className="card-title">{this.state.userAc.username}</span>
                                 {/* data for location goes here */}
-                                <p> location </p>
+                                <p> new </p>
                             </div>
                         </div>
                     </div>
--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -38,6 +38,7 @@ class Login extends Component {
                 .then((res) => {
                     this.setState({ isLoggedIn: true })
                     sessionStorage.setItem('isLoggedIn', true)
+                    sessionStorage.setItem('userId', res.data._id)
                     })
                 .catch(err => console.log(err));
         }
