2ea22b992e01428b4a5e646c5d221f2a6dfd1afb
Marisol G. Martinez <marisol.gabriela.martinez@gmail.com>
2018-07-03 23:40:28 -0700

    update achievements

--- client/src/pages/achievements/achievements.js
+++ client/src/pages/achievements/achievements.js
@@ -1,3 +1,5 @@
+import { Tabs, Tab} from "react-materialize"
+
 import React, {Component} from "react"
 import bear from '../../images/achievements/Bear.png'
 import backpack from '../../images/achievements/Backpack.png'
@@ -21,8 +23,36 @@ import thermocoffee from '../../images/achievements/ThermoCoffee.png'
 import watch from '../../images/achievements/Watch.png'
 import "./achievements.css"
 class Achievements extends Component{
+    //psuedo code//
+    //import api from utils folder
+    //then the api needs to have a function to call the server to query the data base for badges info related to the currently signed in user (think req.params.id)
+    //componentdid(inheriting what the react app has)
+    //call the api to get badges data call .then this.set.state update state to badges data
+    // after state has been updated from our data base call through api, we will pass in the earned badges via this.state.earnedBadges using a forloop
+    //available badges will be handled in a very similiar way 
+
+    //team mates to do's
+    //create the model in mongo
+    //create controller methods for routers
+    //create methods in the front end utils/api to call for badges
+    // might want a seperate routers folder to call on controller functions (optional)
+    //the mern books activity is a great reference for setting this all up
+    state = {
+    user: "",
+    earnedBadges:[],
+    availableBadges: []
+
+}
+
+
+
+
     render (){
         return <div> 
+            <Tabs className='tab-demo z-depth-1'>
+                <Tab title="Earned Badges"></Tab>
+                <Tab title="Unearned Badges" active></Tab>
+            </Tabs>
             <img className="size" src={backpack}/>
             <img className="size" src={bear}/>
             <img className= "size" src={bearpaw}/>
@@ -46,6 +76,9 @@ class Achievements extends Component{
             </div>
 
     }
+      
+    
 }
 
-export default Achievements
+
+export default Achievements;
