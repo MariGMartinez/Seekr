d8cfdad9a4320fc1d9fefcae120cb36e9a4bfccb
unknown <bpham2002@gmail.com>
2018-07-05 13:10:00 -0700

    updated

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const trailRoutes = require("./trails.js");
-const userRoutes = require("./users.js");
-
-// trail routes
-router.use("/trails", trailRoutes);
-router.use("/users", userRoutes);
-
-module.exports = router;
