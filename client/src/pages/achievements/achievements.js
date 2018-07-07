4232bcc4172abb311a7dd3e9ba8ea69cffbda037
chriserb614 <chris.erbstoesser@yahoo.com>
2018-07-07 00:10:17 -0700

    CSS Side-Nav

--- client/src/pages/achievements/achievements.js
+++ client/src/pages/achievements/achievements.js
@@ -1,5 +1,5 @@
 import {Tabs, Tab, Dropdown, Button, NavItem} from "react-materialize"
-
+import Navbar from "../../components/navbar"
 import React, {Component} from "react"
 import bear from '../../images/achievements/Bear.png'
 import backpack from '../../images/achievements/Backpack.png'
@@ -100,7 +100,9 @@ class Achievements extends Component{
 
 
     render (){
-        return <div> 
+        return (
+            <div> 
+                <Navbar />
             <div style={{padding:"20px", height: "300px"}}>
             <div style ={{margin: "0 auto", textAlign:"center", borderRadius:"100%", height:"300px", width:"300px", backgroundColor:"blue"}}>
                 <img src={{}}/>
@@ -168,7 +170,7 @@ class Achievements extends Component{
             <img className="size"src={thermocoffee}/>
             <img className="size"src={watch}/>*/}
             </div>
-
+        )
     }
       
     
